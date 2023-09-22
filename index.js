fb47f42f07cb1c39c33c215a8152c7ff90447927
DidasMba <daniel.mbarushi@gmail.com>
2023-09-22 20:31:57 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply); 
+
+
+const  randomDecimal = Math.random();
+
+const random = Math.floor(randomDecimal * 100) + 1;
+
+console.log(random);
+
+let num3 = 20;
+let num4 = 4;
+let mod = (num3 %= num4) + 4;
+console.log(mod);
+
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+
+console.log(max);
+
